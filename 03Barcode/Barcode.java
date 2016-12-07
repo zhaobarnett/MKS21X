8418fd076d316c52c9df8a2edb4aa5b4414619a8
Barnett Zhao <zhaobarnett@gmail.com>
2016-12-07 06:39:20 -0500

    cw

---
+++ 03Barcode/Barcode.java
@@ -0,0 +1,16 @@
+
+
+public class Barcode implements Comparable<Barcode>{
+    private String _zip;
+    private int _checkDigit;
+
+    public Barcode(String zip){
+	try{
+	    Inte
+	if (zip.length() != 5){
+	    throw new IllegalArgumentException();
+	}
+	
+
+
+}
