8a7b83792c19607c3c42f419668fdafa4bd1ac96
Barnett Zhao <zhaobarnett@gmail.com>
2016-12-29 21:58:36 -0500

    Sorts completed with time checks

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -57,7 +57,7 @@ public class Sorts{
 	//outer loop pervents checking end elements which are already in place
 	int x;
 	for(x = data.length; x > 0; x--){
-	    for(int i = 0; i < x-2; i++){
+	    for(int i = 0; i < x-1; i++){
 		if(data[i] > data[i+1]){
 		    //switching two adjacent elements
 		    int temp = data[i];
@@ -76,7 +76,7 @@ public class Sorts{
 	int[] d = {55};
 
 	//selectionSort
-	//basic
+	//basic with time testing
 	int[] a1 = {7,3,8,1,9,4,6};
 	selectionSort(a1);
 	System.out.println(Arrays.equals(a,a1));
@@ -116,20 +116,52 @@ public class Sorts{
 	//bubbleSort
 	//basic
 	int[] a3 = {7,3,8,1,9,4,6};
-	insertionSort(a3);
+        bubbleSort(a3);
 	System.out.println(Arrays.equals(a,a3));
 	//repeated element
 	int[] b3 = {7,3,8,1,9,4,7,6};
-	insertionSort(b3);
+	bubbleSort(b3);
 	System.out.println(Arrays.equals(b,b3));
 	//empty array
 	int[] c3 = {};
-	insertionSort(c3);
+	bubbleSort(c3);
 	System.out.println(Arrays.equals(c,c3));	
 	//edge case: 1 element
 	int[] d3 = {55};
-	insertionSort(d3);
+	bubbleSort(d3);
 	System.out.println(Arrays.equals(d,d3));
+
+
+	//time testing
+	Random randgen = new Random();
+	int[] test1 = new int[1000];
+	int[] test2 = new int[1000];
+	int[] test3 = new int[1000];
+	for (int i = 0; i < 1000; i++){
+	    int x = randgen.nextInt(1000);
+	    test1[i] = x;
+	    test2[i] = x;
+	    test3[i] = x;
+	}
+
+	//selectionSort
+        long startTime = System.currentTimeMillis();
+	selectionSort(test1);
+	long endTime = System.currentTimeMillis();
+	System.out.println(endTime-startTime);
+	
+	//insertionSort
+        long startTime1 = System.currentTimeMillis();
+	insertionSort(test2);
+	long endTime1 = System.currentTimeMillis();
+	System.out.println(endTime1-startTime1);
+	
+	//bubbleSort
+        long startTime2 = System.currentTimeMillis();
+	bubbleSort(test3);
+	long endTime2 = System.currentTimeMillis();
+	System.out.println(endTime2-startTime2);	
+
     }
 
 }
