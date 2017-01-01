78d9893aef1fd4c09a66328008d116ac8503df52
Barnett Zhao <zhaobarnett@gmail.com>
2016-12-31 22:45:38 -0500

    Window completed

--- GUI/Window.java
+++ GUI/Window.java
@@ -3,10 +3,11 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Window extends JFrame implements ActionListener{
- private Container pane;
- private JButton b;
- private JLabel l;
- private JTextField t;
+
+    private Container pane;
+    private JButton b1;
+    private JButton b2;
+    private JTextField t;
  
   //CONSTRUCTOR SETS EVERYTHING UP
   public Window() {
@@ -16,16 +17,38 @@ public class Window extends JFrame implements ActionListener{
      this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     
      pane = this.getContentPane();
-     pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+     pane.setLayout(new FlowLayout());
     
-     b = new JButton("Convert");
-     l = new JLabel("This is AWESOME! (lies)",null,JLabel.CENTER);
+     b1 = new JButton("Convert F to C");
+     b1.addActionListener(this);
+     b1.setActionCommand("FtoC");
+     
+     b2 = new JButton("Convert C to F");
+     b2.addActionListener(this);
+     b2.setActionCommand("CtoF");
+     
      t = new JTextField(12);
      
-     pane.add(l);
-     pane.add(b);
      pane.add(t);
+     pane.add(b1);
+     pane.add(b2);
  }
+
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+	if(event.equals("FtoC")){
+	    String s = t.getText();
+	    double d = Double.parseDouble(s);
+	    s = Double.toString(tempConverter.FtoC(d));
+	    t.setText(s);
+	}
+	if(event.equals("CtoF")){
+	    String s = t.getText();
+	    double d = Double.parseDouble(s);
+	    s = Double.toString(tempConverter.CtoF(d));
+	    t.setText(s);
+	}
+    }
 
   //MAIN JUST INSTANTIATES + MAKE VISIBLE
   public static void main(String[] args) {
